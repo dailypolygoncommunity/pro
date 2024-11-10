56967ff450b507f9ad7b4fafedb15669e9bbe646
John <godfatherdbanking@gmail.com>
2024-11-10 18:40:54 +0800

    Initial commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'grant-master',
+  location: 'us-central1'
+};
+
